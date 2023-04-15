f416f6f62670c1029500b1af124e05006cfd5027
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-04-14 21:27:21 -0300

    Update cadastro.php

--- tela/cadastro.php
+++ tela/cadastro.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Cadastro</title>
 </head>
-<body>
-    <div>
-        <form action=""></form>
+<body class="bg-dark">
+    <div class="bg-light w-50 mx-auto">
+        <form class="my-5 py-3" action="">
+            <div class="py-2 text-center">
+                <h1>SUA LOGO</h1>
+            </div>
+            <div class="mx-5">
+                <div class="row">
+                    <div class="col">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="nome" id="nome" class="form-control" >
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </body>
 </html>
